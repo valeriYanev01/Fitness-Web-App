db7db3e4df8cdf97573b67af7f11d92c01be6300
valeriYanev01 <valeri.t.yanev@gmail.com>
2024-01-29 14:59:17 +0200

    adding loader

--- frontend/src/components/portalComponents/CreateWorkout.jsx
+++ frontend/src/components/portalComponents/CreateWorkout.jsx
@@ -8,63 +8,71 @@ const CreateWorkout = () => {
   const [exercise, setExercise] = useState("");
   const [weight, setWeight] = useState(null);
   const [reps, setReps] = useState(null);
+  const [error, setError] = useState("");
 
   const { setShowSettings, setLoading } = useContext(WorkoutContext);
   const { user } = useContext(LoginContext);
 
   const newWorkout = () => {
+    setLoading(true);
+    setError("");
     axios
       .post(
         "http://localhost:6969/api/workouts",
         { title: exercise, load: weight, reps },
         { headers: { Authorization: `Bearer ${user.token}` } }
       )
-      .then((data) => {
-        console.log(data);
+      .then(() => {
+        setLoading(false);
+        setError("");
       })
       .catch((err) => {
-        console.log(err);
+        setError(err);
+        setLoading(false);
       });
   };
 
   return (
-    <div className="create-workout">
-      <div className="create-workout-info">
-        <div className="create-workout-info-title">
-          <span>Exercise:</span>
-          <input autoComplete="off" onChange={(e) => setExercise(e.target.value)} />
-        </div>
+    <>
+      {error && <div>{error}</div>}
+      <div className="create-workout">
+        <div className="create-workout-info">
+          <div className="create-workout-info-title">
+            <span>Exercise:</span>
+            <input autoComplete="off" onChange={(e) => setExercise(e.target.value)} />
+          </div>
 
-        <div className="create-workout-info-load">
-          <span>Weight:</span>
-          <input autoComplete="off" onChange={(e) => setWeight(e.target.value)} />
-        </div>
+          <div className="create-workout-info-load">
+            <span>Weight:</span>
+            <input autoComplete="off" onChange={(e) => setWeight(e.target.value)} />
+          </div>
 
-        <div className="create-workout-info-reps">
-          <span>Repetitions:</span>
-          <input autoComplete="off" onChange={(e) => setReps(e.target.value)} />
+          <div className="create-workout-info-reps">
+            <span>Repetitions:</span>
+            <input autoComplete="off" onChange={(e) => setReps(e.target.value)} />
+          </div>
         </div>
+        <span
+          className="create-workout-button"
+          onClick={() => {
+            setShowSettings("Show Workout");
+            newWorkout();
+            setLoading(true);
+          }}
+        >
+          Add
+        </span>
+        <span
+          className="workout-buttons-discard"
+          onClick={() => {
+            setLoading(true);
+            setShowSettings("Show Workout");
+          }}
+        >
+          Cancel
+        </span>
       </div>
-      <span
-        className="create-workout-button"
-        onClick={() => {
-          setShowSettings("Show Workout");
-          newWorkout();
-          setLoading(true);
-        }}
-      >
-        Add
-      </span>
-      <span
-        className="workout-buttons-discard"
-        onClick={() => {
-          setLoading(true);
-          setShowSettings("Show Workout");
-        }}
-      >
-        Cancel
-      </span>
-    </div>
+    </>
   );
 };
 
