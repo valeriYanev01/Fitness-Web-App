ec10c2460240bfdf3cef4275c395ad3a11efecbe
valeriYanev01 <valeri.t.yanev@gmail.com>
2024-02-22 12:53:00 +0200

    a

--- frontend/src/context/ChangeBgContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useState } from "react";
-
-export const ChangeBgContext = createContext({});
-
-export const ChangeBgContextProvider = ({ children }) => {
-  const [selectedItem, setSelectedItem] = useState("");
-
-  return <ChangeBgContext.Provider value={{ selectedItem, setSelectedItem }}>{children}</ChangeBgContext.Provider>;
-};
--- frontend/src/context/MyPortal Page/SettingsContext.jsx
+++
@@ -1,9 +0,0 @@
-import React, { createContext, useState } from "react";
-
-export const SettingsContext = createContext({});
-
-export const SettingsContextProvider = ({ children }) => {
-  const [optionChange, setOptionChange] = useState(null);
-
-  return <SettingsContext.Provider value={{ optionChange, setOptionChange }}>{children}</SettingsContext.Provider>;
-};
